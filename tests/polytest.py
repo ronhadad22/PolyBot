50b8ca2306a827239dad52abdae9d3802c5c4c64
ayam99 <ayam99mousa@gmail.com>
2023-04-01 04:39:23 +0300

    updated

--- tests/polytest.py
+++ tests/polytest.py
@@ -7,7 +7,7 @@ from loguru import logger
 class TestBot(unittest.TestCase):
 
     def setUp(self):
-        with open('../.telegramToken') as f:
+        with open('.telegramToken') as f:
             _token = f.read()
         # create a mock updater object
         self.updater = MagicMock()
